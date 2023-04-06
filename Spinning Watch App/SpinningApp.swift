a9ca626e36bcee3e078e1bca050447e194a1ad4b
David Leyva <dleyvaabrahantes@gmail.com>
2023-04-06 18:23:05 -0400

    Initial Commit

---
+++ Spinning Watch App/SpinningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinningApp.swift
+//  Spinning Watch App
+//
+//  Created by David on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Spinning_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
